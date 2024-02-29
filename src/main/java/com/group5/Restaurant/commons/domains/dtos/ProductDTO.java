4985260e883d636d217396270ba1ad0d9ee1f55c
jeimy555 <yuranyyurbleidys555@gmail.com>
2024-02-29 15:29:32 -0500

    Producto DTO

--- src/main/java/com/group5/Restaurant/commons/domains/dtos/ProductDTO.java
+++ src/main/java/com/group5/Restaurant/commons/domains/dtos/ProductDTO.java
@@ -1,6 +1,25 @@
 package com.group5.Restaurant.commons.domains.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import java.io.Serializable;
 
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
 public class ProductDTO implements Serializable {
+    private String nombre;
+    private categoria categoriaProducto;
+    private String descripcion;
+    private Float precio;
+    private Boolean disponible;
+
+    public enum categoria{
+        hamburguerAndHotDogs,Chiken,fish,meats,desserts,veganFood,kidsMeals;
+    }
 }
