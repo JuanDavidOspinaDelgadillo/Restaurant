d8b47a05988cec567391d397c992d23cf6dbccf3
Jessika Mariana Hernandez Chamucero <jessika.hernandez@globant.com>
2024-03-11 20:10:23 -0500

    RESTAURANT005- Delete Customer by document PART 1

--- src/main/java/com/group5/Restaurant/services/interfaces/IClientService.java
+++ src/main/java/com/group5/Restaurant/services/interfaces/IClientService.java
@@ -10,4 +10,5 @@ import org.springframework.http.ResponseEntity;
 public interface IClientService {
     ResponseEntity<ObjectResponseDTO> createClient(ClientDTO clientDTO);
     ResponseEntity<ObjectResponseDTO> readClient(String clientDocument);
+    ResponseEntity<ObjectResponseDTO> deleteClient(String clientDocument);
 }
